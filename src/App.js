8064339f7d8f20ca9e3e30e8bb4b821403a7681b
Kamlesh Khatod <96969057+kamlesh3006@users.noreply.github.com>
2025-07-23 22:32:41 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Background from './Components/Background';
+import colorImage from './assets/front.png'; 
+import bwImage from './assets/back.png';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Background
+        bottomImageSrc={colorImage}
+        topImageSrc={bwImage}
+        alt="A landscape view"
+        revealRadius={120} /* Optional: you can change the circle size */
+      />
     </div>
   );
 }
